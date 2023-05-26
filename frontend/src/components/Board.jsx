9e531741445da2617684398dd055cf2af8f72bb4
aguusoleti <agustin_soleti@hotmail.com>
2023-05-26 19:57:55 -0300

    fix board

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import BoardOptions from "./modal/BoardOptions";
 
 const Board = (props) => {
+
   const [showModal, setShowModal] = useState(false); // Estado para controlar la visibilidad del modal
-console.log(showModal)
   const handleClick = () => {
     setShowModal(true); // Al hacer clic, muestra el modal estableciendo el estado a true
   };
 
   return (
-    <main >
-      <button onClick={handleClick} className="bg-[#FFFFF] w-[90vw] h-[20.5vh] text-black flex flex-col items-star rounded-3xl border-2 border-#D4D4D8 shadow-lg mt-1">
+    <main>
+      <button
+        onClick={handleClick}
+        className="bg-[#FFFFF] w-[90vw] h-[20.5vh] text-black flex flex-col items-star rounded-3xl border-2 border-#D4D4D8 shadow-lg mt-1"
+      >
         <h2 className="w-[90vw] text-[1.5em] text-start text-700 font-bold mt-3 ml-3 ">
           {props.title}
         </h2>
@@ -19,8 +22,10 @@ console.log(showModal)
           Careado hace {props.data} días
         </h3>
       </button>
-      {showModal && <BoardOptions title={props.title}  onClose={() => setShowModal(false)}/>}
-      {/* Renderiza el modal solo si showModal es true */}
+      {showModal && (
+        <BoardOptions title={props.title} onClose={() => setShowModal(false)} />
+      )}
+      
     </main>
   );
 };
