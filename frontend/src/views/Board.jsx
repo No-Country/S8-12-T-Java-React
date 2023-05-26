98907bb86b50e0c3ec3af0112611957d994b9f80
aguusoleti <agustin_soleti@hotmail.com>
2023-05-26 17:18:13 -0300

    fix board

--- frontend/src/views/Board.jsx
+++ frontend/src/views/Board.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Board from "../components/Board";
+import NewBoard from "../components/modal/NewBoard";
 
 export default function Boards() {
-  const boardTitle = "Título del tablero";
-  const boardData = "4";
-  const boardDescription = "El trabajo de nuestras vida";
-
   const boards = [
     {
       boardTitle: "Titulo 1",
@@ -34,11 +30,11 @@ export default function Boards() {
       boardDescription: "El trabajo de nuestras vidas",
     },
   ];
-
+ 
   return (
     <>
-      <main className="bg-white text-black w-full h-full flex flex-col items-center">
-        <div className="w-[90vw] text-[1.28em] text-star text-700 font-bold flex flex-row items-center mt-3 mb-3">
+      <main className="bg-white text-black w-full h-full flex flex-col items-center ">
+        <div className="w-[90vw] text-[1.28em] text-star text-700 font-bold flex flex-row items-center mt-10 mb-3">
           <img
             className="w-[10vw] drop-shadow-md mr-3 "
             src="../../public/trello.svg"
@@ -46,20 +42,15 @@ export default function Boards() {
           ></img>
           <h2>Tablero</h2>
         </div>
-        <Link to={"login"}>
-          <button className="w-[90vw] h-[6vh] bg-[#6D28D9] text-white font-normal">
-            Nuevo tablero
-          </button>
-        </Link>
-        {boards.map((board,index)=>(
-
-          <div className="m-5">
+        <NewBoard />
+        {boards.map((board, index) => (
+          <div className="m-3">
             <Board
-            key={index}
+              key={index}
               title={board.boardTitle}
-              data={board.boardDescription}
+              data={board.boardData}
               description={board.boardDescription}
-              />
+            />
           </div>
         ))}
       </main>
