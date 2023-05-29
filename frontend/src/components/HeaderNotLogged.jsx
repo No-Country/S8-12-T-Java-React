2343abb2f7ce787b42a50f355b9aea9570d16296
Jiao <60641358+Jiao-lin@users.noreply.github.com>
2023-05-28 23:10:49 -0300

    try to fix

--- frontend/src/components/HeaderNotLogged.jsx
+++ frontend/src/components/HeaderNotLogged.jsx
@@ -8,11 +8,11 @@ export const HeaderNotLogged = () => {
   const [render, setrender] = useState(false);
   return (
     <>
-    <header className='bg-neutral-50 w-full h-[10vh] flex flex-row items-center justify-between drop-shadow-lg fixed z-10'>
+    <header className='bg-neutral-50 w-full h-[10vh] flex flex-row items-center justify-between drop-shadow-lg fixed z-50'>
         <h1 className='text-[1.25em] text-neutral-600 antialiased ml-[3vw]'>Career<span className='text-orange-600'>Watch</span></h1>
         <button onClick={()=>{setrender(!render)}} className='h-[2.8em] w-[2.8em] flex items-center justify-center mr-[5.5vw] border border-neutral-900 rounded-md '><HamburgHeader/></button>
     </header>
-    {render ? <MenuNotLogged /> : null}
+    {render ? <MenuNotLogged render={render} setrender={setrender}/> : null}
     
     <div className="h-[10vh]"/>
     </>
