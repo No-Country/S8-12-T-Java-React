84027f7e230896765c71190194ed28f7d2fc5cdb
aguusoleti <agustin_soleti@hotmail.com>
2023-05-28 22:27:43 -0300

    fix bug

--- frontend/src/components/HeaderNotLogged.jsx
+++ frontend/src/components/HeaderNotLogged.jsx
@@ -5,7 +5,7 @@ import '../styles/fontLogo.css'
 export const HeaderNotLogged = () => {
   return (
     <>
-    <header className='bg-neutral-50 w-full h-[10vh] z-50 flex flex-row items-center justify-between drop-shadow-lg fixed z-10'>
+    <header className='bg-neutral-50 w-full h-[10vh] z-50 flex flex-row items-center justify-between drop-shadow-lg fixed'>
         <h1 className='text-[1.25em] text-neutral-600 antialiased ml-[3vw]'>Career<span className='text-orange-600'>Watch</span></h1>
         <button className='h-[2.4em] w-[2.8em] flex items-center justify-center mr-[2.5vw] border border-neutral-900 rounded-md '><HamburgHeader/></button>
     </header>
--- frontend/src/views/Logged.jsx
+++ frontend/src/views/Logged.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Boards from './Board'
-import { Footer } from '../components/Footer'
-import { HeaderNotLogged } from '../components/HeaderNotLogged'
-import { Route, Routes } from 'react-router-dom'
-
+import React from "react";
+import Boards from "./Board";
+import { Footer } from "../components/Footer";
+import { HeaderNotLogged } from "../components/HeaderNotLogged";
+import { Route, Routes } from "react-router-dom";
+import Perfil from "./Perfil";
 
 export default function Logged() {
   return (
     <>
-        <HeaderNotLogged/>
-        <Routes>
-            <Route element={<Boards/>} path='/'/>
-            <Route element={<Boards/>} path='*'/>
-        </Routes>
-        <Footer/>
+      <HeaderNotLogged />
+      <Routes>
+        <Route element={<Boards />} path="/" />
+        <Route element={<Boards />} path="*" />
+        <Route element={<Perfil />} path="/perfil" />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
