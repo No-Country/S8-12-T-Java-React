8de7038793cd92bd925eebad4b88ba1ef0482d76
Jiao <60641358+Jiao-lin@users.noreply.github.com>
2023-05-30 00:54:15 -0300

    SP3-CW84: Fix Error Nesting Button En Board

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Button = (props) => {
   return (
-    <div>
-          <Link to={props.url}><button className='w-[90vw] h-[6vh] bg-[#6D28D9] text-white font-normal rounded-lg drop-shadow-md
-          '>{props.name}</button></Link>
+    <div className='w-auto h-auto'>
+          <Link to={props.url} className='w-[90vw] h-[6vh] flex items-center justify-center bg-[#6D28D9] text-white font-normal rounded-lg drop-shadow-md
+          '>{props.name}</Link>
     </div>
   )
 }
