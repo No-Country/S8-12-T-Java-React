a29d2b11e9944a55bcf08d3e6f4b66993df7149e
Nicolás <nicolaseugroberto@gmail.com>
2023-05-31 00:57:07 -0300

    Componente welcome page

--- frontend/src/views/Logged.jsx
+++ frontend/src/views/Logged.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Boards from "./Board";
 import { Footer } from "../components/Footer";
-import { HeaderLogged } from '../components/HeaderLogged'
+import { HeaderLogged } from "../components/HeaderLogged";
 import { Route, Routes } from "react-router-dom";
 import Perfil from "./Perfil";
-
+import WelcomePage from "./WelcomePage";
 
 export default function Logged() {
   return (
     <>
-        <HeaderLogged/>
-        <Routes>
-            <Route element={<Boards/>} path='/'/>
-            <Route element={<Boards/>} path='*'/>
-            <Route element={<Perfil />} path="/curriculumn" />
-        </Routes>
-        <Footer/>
+      <HeaderLogged />
+      <Routes>
+        <Route element={<Boards />} path="/" />
+        <Route element={<Boards />} path="*" />
+        <Route element={<Perfil />} path="/curriculumn" />
+        <Route element={<WelcomePage />} path="/welcomePage" />
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/views/WelcomePage.jsx
@@ -0,0 +1,19 @@
+import comprobado from "../../public/comprobado.svg";
+
+export default function WelcomePage() {
+  return (
+    <div className="p-4 my-6 h-[50vh] flex flex-col justify-around content-center">
+      <img className="h-40" src={comprobado} alt="Tilde de comprobado" />
+      <p className="text-lg font-bold text-center">
+        ¡Bienvenido a CareerWatch!
+      </p>
+      <p className="px-4 text-base text-center">
+        Ya puedes ingresar a tu tablero y comenzar a organizarte para encontrar
+        tu trabajo ideal.
+      </p>
+      <button className="shadow-md rounded bg-[#6D28D9] text-white h-10 w-full">
+        Ir al tablero
+      </button>
+    </div>
+  );
+}
