b12f1e715f6c15b3c568e9b0f7947a0f234c13e2
Nicolás <nicolaseugroberto@gmail.com>
2023-06-06 19:44:34 -0300

    Responsive primer container

--- frontend/src/views/Landing.jsx
+++ frontend/src/views/Landing.jsx
@@ -12,30 +12,28 @@ import calendario from "../assets/images/Calendario.svg";
 export default function Landing() {
   return (
     <>
-      <div className="bg-neutral-100 text-black w-full flex flex-col justify-evenly items-center">
-        <div className="h-[90vh] mt-[4vh] flex flex-col justify-evenly items-center">
-          <img
-            className="h-1/3 drop-shadow-md"
-            src={heroIMG}
-            alt="Persona con laptop"
-          ></img>
-          <h2 className='w-[90vw] text-[1.25em] text-center text-neutral-900 font-["Lato","sans-serif"] font-bold'>
-            Lorem ipsum dolor sit amet
-            <br />
-            consectetur. Lacinia imperdiet
-            <br />
-            consectetur nunc a tortor quam.
-          </h2>
-          <h3 className='w-[90vw] text-[1em] text-center text-neutral-900 font-["Lato","sans-serif"] font-normal'>
-            Lorem ipsum dolor sit amet consectetur.
-            <br />
-            Cras facilisi quam.
-          </h3>
-          <Link to={"register"}>
-            <button className='w-[90vw] h-[6vh] bg-[#6D28D9] text-white font-["Lato","sans-serif"] font-normal rounded-lg'>
-              Registrarse gratis
-            </button>
-          </Link>
+      <div className="bg-neutral-100 text-black w-full flex flex-col justify-evenly items-center px-4 sm:px-10">
+        <div className="h-[90vh] w-full flex flex-col justify-evenly items-center sm:flex-row-reverse">
+          <div className="h-3/6 sm:h-3/4 sm:w-6/12">
+            <img
+              className="h-full drop-shadow-md mx-auto"
+              src={heroIMG}
+              alt="Persona con laptop"
+            ></img>
+          </div>
+          <div className="w-11/12 flex flex-col justify-evenly sm:content-start sm:w-6/12 sm:mx-auto">
+            <h2 className='my-2 w-full text-[1.25em] sm:text-[2.25em] text-center text-neutral-900 font-["Lato","sans-serif"] font-bold sm:text-left'>
+              Controla tu camino hacia el éxito en un solo lugar.
+            </h2>
+            <h3 className='my-2 w-full text-[1em] text-center text-neutral-900 font-["Lato","sans-serif"] font-normal sm:text-left'>
+              Organiza, destaca y progresa con nuestra completa solución.
+            </h3>
+            <Link to={"register"}>
+              <button className='my-2 w-full h-10 bg-[#6D28D9] text-white font-["Lato","sans-serif"] font-normal rounded-lg shadow-lg sm:w-72'>
+                Registrarse gratis
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="h-[75vh] flex flex-col justify-between items-center my-[9vh]">
           <h2 className='w-[90vw] text-[1.15em] text-center text-neutral-900 font-["Lato","sans-serif"] font-bold'>
