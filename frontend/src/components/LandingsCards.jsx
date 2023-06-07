d554504d5cc7eac02f3d930cf447e7c5f56cdeef
Nicolás <nicolaseugroberto@gmail.com>
2023-06-06 21:52:52 -0300

    Fix fuente

--- frontend/src/components/LandingsCards.jsx
+++ frontend/src/components/LandingsCards.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import "../styles/latoFont.css";
 
 export const LandingsCards = ({ img, title }) => {
   return (
     <div className="bg-neutral-50 flex flex-col items-center justify-around w-full sm:justify-center sm:w-[191px] h-[150px] sm:h-[191px] rounded-2xl shadow-md border border-neutral-300">
       <img className="w-20 m-1" src={img} alt={title} />
-      <h2 className='text-[1.1em] text-center text-neutral-900 font-["Lato","sans-serif"] font-bold'>
+      <h2 className="text-[1.1em] text-center text-neutral-900 font-bold">
         {title}
       </h2>
     </div>
