fefa55909adee499d51b50d49955c9e2745af562
Nicolás <nicolaseugroberto@gmail.com>
2023-06-08 15:23:58 -0300

    button width

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
   return (
     <Link
       to={props.url}
-      className=" rounded w-[90vw] bg-[#6D28D9] text-white h-10 shadow-md flex items-center justify-center"
+      className=" rounded w-full bg-[#6D28D9] text-white h-10 shadow-md flex items-center justify-center"
       onClick={props.onClick}
     >
       {props.name}
